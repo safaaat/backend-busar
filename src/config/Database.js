2cfa5815167f99cb13368ada162b987209c831f9
Muhammad Safaat <paatlupi01@gmail.com>
2023-11-26 23:57:18 +0700

    change DataBase

--- src/config/Database.js
+++ src/config/Database.js
@@ -2,14 +2,14 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-// const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//     host: process.env.DB_HOST,
-//     dialect: "mysql"
-// });
-
-const db = new Sequelize("buah_sayur", "root", "", {
-    host: "localhost",
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: "mysql"
-})
+});
+
+// const db = new Sequelize("buah_sayur", "root", "", {
+//     host: "localhost",
+//     dialect: "mysql"
+// })
 
 export default db;
