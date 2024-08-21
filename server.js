5bfff9d87d87eeffe12d8a39cc71cd02c3ad38af
Muhammad Safaat <paatlupi01@gmail.com>
2024-08-21 09:21:34 +0700

    update

--- server.js
+++ server.js
@@ -23,8 +23,7 @@ try {
 }
 
 app.use((req, res, next) => {
-    // res.setHeader('Access-Control-Allow-Origin', "*");
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader('Access-Control-Allow-Origin', process.env.FRONT_END_URL);
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
